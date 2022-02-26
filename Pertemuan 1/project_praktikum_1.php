659b334fad49db8584458bbfc75ea476e37baf57
rizqi10082002 <fauzan.ardsh@gmail.com>
2022-02-26 21:06:41 +0700

    php

---
+++ Pertemuan 1/project_praktikum_1.php
@@ -0,0 +1,27 @@
+<?php
+  $array_buah = ["Pepaya","Mangga","Pisang","Jambu"];
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style (1).css">
+  <title>Document</title>
+</head>
+<body>
+    <?php foreach ($array_buah as $buah):?>
+    <article class="holder">
+      <section class="card">
+        <div class="card__text">
+          <h2><?= $buah?></h2>
+        </div>
+      </section>
+    </article>
+    <?php endforeach; ?>
+  </div>
+
+</body>
+</html>
