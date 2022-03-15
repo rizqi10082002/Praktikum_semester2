37863135e174048022502afc72b734b358324e0c
rizqi10082002 <fauzan.ardsh@gmail.com>
2022-03-15 15:02:21 +0700

    Tugas 3

---
+++ Pertemuan 3/Tugas 3/view/artikel1.php
@@ -0,0 +1,10 @@
+<?php
+include_once '../layout/header.php';
+?>
+
+<h1>Ini Artikel 1</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione quos, incidunt libero error id dolore debitis qui, vel eos veritatis nam. Explicabo totam culpa quas fugit exercitationem architecto accusamus dignissimos.</p>
+
+<?php
+include_once '../layout/footer.php';
+?>
